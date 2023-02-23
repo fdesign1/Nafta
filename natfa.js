e8174bfbd93f0ca344510c15f0b7f0c44bfc8be5
Mati <matiasferreras33@gmail.com>
2023-02-23 02:41:51 +0100

    added manual

--- natfa.js
+++ natfa.js
@@ -1,8 +1,10 @@
 const myFunction = () =>{
+    //pregunta 1
     var Litros = prompt('Cuantos Litros de combustible repostaste?');
-
+    //pregunta 2
     var Precio = prompt('Ingresa el precio del combustible por Litro');
     if  (Litros >= 0){
+        //ecuacion de litros por el precio del combustible 
         document.getElementById('GastoTable').innerHTML = `Gastaste: ${Litros*Precio} Zlotys`
         document.getElementById('CombustibleRep').innerHTML =`${Litros} Litros`
     }
@@ -10,7 +12,7 @@ const myFunction = () =>{
         document.getElementById('GastoTable').innerHTML ='el valor que ingresaste es incorrecto'
         document.getElementById('CombustibleRep').innerHTML ='el valor que ingresaste es incorrecto'
     }
-    //distancia>>>>>>>>>>>>>>>>>>>>>
+    //preguta 3 distancia>>>>>>>>>>>>>>>>>>>>>
     var Distance= prompt('Que distancia vas a recorrer?')
     if (Distance >= 0){
         document.getElementById('ConsumoTable').innerHTML =`A esa distancia consumira: ${(Distance * Precio) / 100} Litros`
@@ -18,19 +20,17 @@ const myFunction = () =>{
     }
     else{
         document.getElementById('ConsumoTable').innerHTML ='el valor que ingresaste es incorrecto'
-        document.getElementById('DistanciaRec').innerHTML =`${Distance} PLN`
     }
   
-    //Kilometraje
+    //Pregunta 4 Kilometraje
     var CambioAceite = 132299;
   var Kmactual = prompt('Cuantos km tiene el auto actualmente');
   if (Kmactual >= 0){
-document.getElementById('Total').innerHTML = `haz recorrido:${Kmactual - CambioAceite} kms`}
-
-else {
-    document.getElementById('Total').innerHTML =`No sabe`
-}
 document.getElementById('KmActuals').innerHTML = Kmactual;
+document.getElementById('Total').innerHTML = `haz recorrido:${Kmactual - CambioAceite} kms`}
+else {
+    document.getElementById('KmActuals').innerHTML =`El valor q ingresaste es incorrecto`
+}
   }
 
 
