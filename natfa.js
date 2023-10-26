45dd922711ec2f7c5f0607360a96b9cfba5241a5
Mati <matiasferreras33@gmail.com>
2023-10-27 01:05:59 +0200

    new updates

--- natfa.js
+++ natfa.js
@@ -1,34 +1,56 @@
 const myFunction = () =>{
-    var Litros = prompt('Cuantos Litros de combustible repostaste?'); //pregunta 1 Litros
-    var Precio = prompt('Podes Ingresar el precio del combustible por Litro?'); //pregunta 2   --- Precio combustible por 1 litro
+    let Litros = prompt('Cuantos Litros de combustible repostaste?'); //pregunta 1 Litros
+    let Precio = prompt('De cuanto es el precio del combustible por Litro?'); //pregunta 2   --- Precio combustible por 1 litro
+    let Moneda = prompt('Ingresa el nombre de tu Moneda (ejemplo: Peso, Euro, Zlotych');
+
+
+
     if  (Litros >= 0){
-        document.getElementById('GastoTable').innerHTML = `Gastaste: ${Litros*Precio} Zlotys` //ecuacion de litros por el precio del combustible 
+        document.getElementById('GastoTable').innerHTML = `Gastaste: ${Litros*Precio} ${Moneda}` //ecuacion de litros por el precio del combustible 
         document.getElementById('CombustibleRep').innerHTML =`${Litros} Litros` //No es Importante solo ingresa lo del prompt en la tabla directamente
     }
     else{
         document.getElementById('GastoTable').innerHTML ='el valor que ingresaste es incorrecto'
         document.getElementById('CombustibleRep').innerHTML ='el valor que ingresaste es incorrecto'
     }
-    var Consumo = prompt('Cuanto consume el auto cada 100km?')  //preguta 3 Consumo ----------------------->
-    var Distance= prompt('Que distancia vas a recorrer?')  //Pregunta 4
+    
+    let Consumo = prompt('Cuanto consume el auto cada 100km?')  //preguta 3 Consumo ----------------------->
+    let Distance= prompt('Que distancia vas a recorrer?')  //Pregunta 4
     if (Distance > 0){
         document.getElementById('ConsumoTable').innerHTML =`A esa distancia consumira: ${(Distance * Consumo) / 100} Litros` //Ecuacion consumo
-        document.getElementById('GastoKm').innerHTML = `Vas a gastar : ${(Distance*Consumo/100)*Precio} Zlotys` //Ecuacion Gasto
+        document.getElementById('GastoKm').innerHTML = `Vas a gastar: ${(Math.round(Distance*Consumo/100)*Precio)} ${Moneda}` //Ecuacion Gasto
         document.getElementById('DistanciaRec').innerHTML =`${Distance} Km` //No es Importante solo ingresa lo del prompt en la tabla directamente
     }
     else{
         document.getElementById('ConsumoTable').innerHTML ='el valor que ingresaste es incorrecto'
     }
-  var CambioAceite = 132299;
-  var Kmactual = prompt('Cuantos km tiene el auto actualmente');   //Pregunta acerca del Kilometraje
-  if (Kmactual > 0){
-document.getElementById('KmActuals').innerHTML = Kmactual; //pone los Km actuales en la tabla html
-document.getElementById('ProxCambio').innerHTML = `Cambiar Aceite en :${Kmactual - CambioAceite} kms`} //Total de Km recorridos
-else {
-    document.getElementById('KmActuals').innerHTML =`El valor q ingresaste es incorrecto`
-}
+  let CambioAceite = 132299;
+
+  let Confirmar = confirm(' Queres calcular tambien cuando tenes que cambiar el aceite?');
+  if (Confirmar === true){
+    let Kmactual = prompt('Cuantos km tiene el auto actualmente');   //Pregunta acerca del Kilometraje
+    if (Kmactual > 0){
+  document.getElementById('KmActuals').innerHTML = Kmactual; //pone los Km actuales en la tabla html
+  document.getElementById('ProxCambio').innerHTML = `Cambiar Aceite en: ${Kmactual - CambioAceite} Kms`} //Total de Km recorridos
+  else {
+      document.getElementById('KmActuals').innerHTML =`El valor q ingresaste es incorrecto`
+    
+  }
+
+
+  }
+
+
 }
 
+
+function reset(){
+    var td = document.getElementsByTagName('td');
+
+    for(var i = 0; i < td.length; i++){
+        td[i].innerHTML = ''
+    }
+}
 
   
 
