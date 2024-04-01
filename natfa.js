13b85759081c0c9c203908ff5c33a097de657f9e
Mati <matiasferreras33@gmail.com>
2024-04-01 20:23:40 +0200

    mejora del codigo en general

--- natfa.js
+++ natfa.js
@@ -1,8 +1,11 @@
+
+
+
 const myFunction = () =>{
     let Litros = prompt('Cuantos Litros de combustible repostaste?'); //pregunta 1 Litros
     let Precio = prompt('De cuanto es el precio del combustible por Litro?'); //pregunta 2   --- Precio combustible por 1 litro
-    let Moneda = prompt('Ingresa el nombre de tu Moneda (ejemplo: Peso, Euro, Zlotych');
-
+    let Moneda = 'Pln'
+    let Consumo = 7;
 
 
     if  (Litros >= 0){
@@ -14,7 +17,7 @@ const myFunction = () =>{
         document.getElementById('CombustibleRep').innerHTML ='el valor que ingresaste es incorrecto'
     }
     
-    let Consumo = prompt('Cuanto consume el auto cada 100km?')  //preguta 3 Consumo ----------------------->
+
     let Distance= prompt('Que distancia vas a recorrer?')  //Pregunta 4
     if (Distance > 0){
         document.getElementById('ConsumoTable').innerHTML =`A esa distancia consumira: ${(Distance * Consumo) / 100} Litros` //Ecuacion consumo
@@ -24,14 +27,21 @@ const myFunction = () =>{
     else{
         document.getElementById('ConsumoTable').innerHTML ='el valor que ingresaste es incorrecto'
     }
+
+
+
+
+    //CAMBIO ACEITE 
+
+    
   let CambioAceite = 132299;
 
   let Confirmar = confirm(' Queres calcular tambien cuando tenes que cambiar el aceite?');
   if (Confirmar === true){
-    let Kmactual = prompt('Cuantos km tiene el auto actualmente');   //Pregunta acerca del Kilometraje
-    if (Kmactual > 0){
-  document.getElementById('KmActuals').innerHTML = Kmactual; //pone los Km actuales en la tabla html
-  document.getElementById('ProxCambio').innerHTML = `Cambiar Aceite en: ${Kmactual - CambioAceite} Kms`} //Total de Km recorridos
+    let Kmactuales = prompt('Cuantos km tiene el auto actualmente?');   //Pregunta acerca del Kilometraje
+    if (Kmactuales > 0){
+  document.getElementById('KmActuals').innerHTML = Kmactuales; //pone los Km actuales en la tabla html
+  document.getElementById('ProxCambio').innerHTML = `Cambiar Aceite en: ${13000 - (Kmactuales - CambioAceite)} Kms`} //Total de Km recorridos
   else {
       document.getElementById('KmActuals').innerHTML =`El valor q ingresaste es incorrecto`
     
@@ -42,6 +52,8 @@ const myFunction = () =>{
 
 
 }
+
+//LIMPIAR TABLA 
 
 
 function reset(){
